ab244ba46618645ba95673ccbd1a3127ef58ab0d
slman97 <slmanismael20@gmail>
2024-04-04 16:43:55 +0300

    laravel 8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,9 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            'firstname' => 'ali',
+            'lastname' => 'ahmad',
+            "email" =>  "ali@email.com",
+            "Phone" =>  "0974125896",
+            "password" =>  Hash::make('123456789'),
+        ]);
+        DB::table('users')->insert([
+            'firstname' => 'ahmad',
+            'lastname' => 'ahmad',
+            'user_type'=> 'admin',
+            "email" =>  "ahmad@email.com",
+            "Phone" =>  "0963258741",
+            "password" =>  Hash::make('123456789'),
         ]);
     }
 }
